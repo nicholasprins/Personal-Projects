5a2c677040a1ea3300ef330fd9e3f3a7e42662df
Nick <59611015+nprinslo@users.noreply.github.com>
2020-04-03 19:15:24 +0200

    Add files via upload

---
+++ learning python/if_elif_example.py
@@ -0,0 +1,10 @@
+name='bob'
+age=3000
+if name == 'alice':
+    print('hi alice')
+elif age < 12:
+    print(' you are not alice kiddo')
+elif age > 2000:
+    print(' unlike you, alice is not an undead immortal')
+elif age > 100:
+    print('you are not alice, granny')
